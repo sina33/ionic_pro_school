b39384a33cabefb2c3b97bf8db1087c1877a94f8
sina <sina.saeedi.edu@gmail.com>
2018-06-07 20:21:10 +0430

    api provider added

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 /*
@@ -9,9 +9,44 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class ApiProvider {
+  url: string = 'https://jsonplaceholder.typicode.com/photos';
 
   constructor(public http: HttpClient) {
     console.log('Hello ApiProvider Provider');
+  }
+
+  get(endpoint: string, params?: any, reqOpts?: any) {
+    if (!reqOpts) {
+      reqOpts = {
+        params: new HttpParams()
+      };
+    }
+
+    // Support easy query params for GET requests
+    if (params) {
+      reqOpts.params = new HttpParams();
+      for (let k in params) {
+        reqOpts.params = reqOpts.params.set(k, params[k]);
+      }
+    }
+
+    return this.http.get(this.url + '/' + endpoint, reqOpts);
+  }
+
+  post(endpoint: string, body: any, reqOpts?: any) {
+    return this.http.post(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  put(endpoint: string, body: any, reqOpts?: any) {
+    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  delete(endpoint: string, reqOpts?: any) {
+    return this.http.delete(this.url + '/' + endpoint, reqOpts);
+  }
+
+  patch(endpoint: string, body: any, reqOpts?: any) {
+    return this.http.patch(this.url + '/' + endpoint, body, reqOpts);
   }
 
 }
--- src/providers/news/news.ts
+++ src/providers/news/news.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { HttpClientModule } from "@angular/common/http";
+
+import { News } from "../../models/news";
+import { ApiProvider } from "../api/api";
 
 /*
   Generated class for the NewsProvider provider.
@@ -14,32 +15,18 @@ export class NewsProvider {
   urlAddress: any = "http://api.jsonbin.io/b/5b129bea7a973f4ce57850f5"
   posts: any
 
-  constructor(public http: HttpClient
-    // , private httpClient: HttpClientModule) {
-  ){
+  constructor(public api: ApiProvider) {
     console.log('Hello NewsProvider Provider');
   }
 
-  // getData() {
-  //   this.httpClient.get(this.urlAddress).subscribe(res => {
-  //     this.posts = res
-  //   })
-  // }
+  query(params?: any) {
+    return this.api.get('/item', params);
+  }
 
-// this.http.get('http://ionic.io', {}, {})
-//   .then(data => {
+  add(item: News) {
+  }
 
-//     console.log(data.status);
-//     console.log(data.data); // data received by server
-//     console.log(data.headers);
-
-//   })
-//   .catch(error => {
-
-//     console.log(error.status);
-//     console.log(error.error); // error message as string
-//     console.log(error.headers);
-
-//   });
+  delete(item: News) {
+  }
 
 }
